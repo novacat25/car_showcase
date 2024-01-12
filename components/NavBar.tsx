6d230ab6e3e3cb6929459f20ee2c1629c0ec2e97
NovaCat <sparkstorms2@gmail.com>
2024-01-12 15:55:29 +0900

    Feat: add a logo image

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,25 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <header className="w-full absolute z-10">
+        <nav className="max-w-[1440px] mx-auto
+        flex justify-between items-center
+        sm:px-16 px-6 py-4">
+            <Link href="/" className='flex
+            justify-center items-center'>
+                <Image
+                    src="/logo.svg"
+                    alt="Car Hub Logo"
+                    width={118}
+                    height={18}
+                    className="object-contain"
+                />
+            </Link>
+        </nav>
+    </header>
   )
 }
 
