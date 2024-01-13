02e06cd71f13087a72b0fb7a1d802d0fbdcb9515
SpaceFox75 <sparkstorms2@gmail.com>
2024-01-13 14:31:02 +0900

    revert: Image(TODO: clear this problem on fix branch)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -30,17 +30,11 @@ const Hero = () => {
       </div>
       <div className="hero__image-container">
         <div className="hero__image">
-          {/* TODO: set sizes="100%" in this tag. */}
-          <Image 
-            src="/hero.png" 
-            alt="hero"
-            sizes="100%"
-            fill 
-            className="object-contain" 
-          />
+          <Image src="/hero.png" alt="hero"
+          fill className="object-contain" />
+
+          <div className="hero__image-overlay" />
         </div>
-        
-        <div className="hero__image-overlay" />
       </div>
     </div>
   )
