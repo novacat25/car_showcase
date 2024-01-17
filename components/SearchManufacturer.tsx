0ad250b320a0f7ce19ee7ac5ead2d94d4b1dff83
NovaCat <sparkstorms2@gmail.com>
2024-01-17 11:36:03 +0900

    Feat: make basic design by using headless-UI

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,10 +1,43 @@
-import { SearchManufacturerProps } from '@/types'
-import React from 'react'
+"use client"
 
-const SearchManufacturer = ({ manufacturer, setManufacturer }:SearchManufacturerProps) => {
-  return (
-    <div>SearchManufacturer</div>
-  )
+import { Combobox, Transition } from '@headlessui/react'
+import Image from 'next/image'
+
+import { SearchManufacturerProps } from '@/types'
+import React, { Fragment, useState } from 'react'
+
+const SearchManufacturer = ({ manufacturer,
+  setManufacturer }:SearchManufacturerProps) => {
+    const [query,setQuery] = useState('')
+
+    
+    return (
+      <div className="search-manufacturer">
+        <Combobox>
+          <div className="relative w-full">
+            <Combobox.Button className="absolute top-
+            [14px]">
+              <Image
+                src="/car-logo.svg"
+                width={20}
+                height={20}
+                className="ml-4"
+                alt="Car Logo"
+              />
+            </Combobox.Button>
+
+            <Combobox.Input 
+              className="search-manufacturer__input"
+              placeholder="Volkswagen"
+              displayValue={(manufacturer: string) => 
+                manufacturer
+              }
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
+            />
+          </div>
+        </Combobox>
+      </div>
+    )
 }
 
 export default SearchManufacturer
