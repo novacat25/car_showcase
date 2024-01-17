20ea946e0321034efdbe961f3ffaf4255d3ffa34
NovaCat <sparkstorms2@gmail.com>
2024-01-17 13:33:15 +0900

    Feat: add props on Combobox component

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -21,7 +21,7 @@ const SearchManufacturer = ({ manufacturer,
 
     return (
       <div className="search-manufacturer">
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
           <div className="relative w-full">
             <Combobox.Button className="absolute top-
             [14px]">
