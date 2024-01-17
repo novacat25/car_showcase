4711a8888ce952992be0000fed6b2561caca0d9d
NovaCat <sparkstorms2@gmail.com>
2024-01-17 13:31:50 +0900

    Feat: implement autocomplete combobox

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -61,7 +61,25 @@ const SearchManufacturer = ({ manufacturer,
                     }
                       value={item}
                     >
-                      {item}
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                      </>
+                    )}
                     </Combobox.Option>
                   )
                 )}
