dd2c01791d2efe42070e0ef20cd96b3917701596
SpaceFox75 <sparkstorms2@gmail.com>
2024-01-20 14:45:47 +0900

    Feat: add images

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,5 +1,6 @@
 import { CarProps } from '@/types'
 import { calculateCarRent } from '@/utils'
+import Image from 'next/image'
 import React from 'react'
 
 interface CarCardProps {
@@ -33,6 +34,18 @@ const CarCard = ({ car }: CarCardProps) => {
           /day
         </span>
       </p>
+
+      <div className="relative w-full h-40 my-3
+      object-contain">
+        <Image
+          src="/hero.png"
+          alt="car model"
+          sizes='100%'
+          fill
+          priority
+          className="object-contain"
+        />
+      </div>
     </div>
   )
 }
