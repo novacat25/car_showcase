c437bfa358cccf8f7d00ae3a1bf89f883117e8d0
SpaceFox75 <sparkstorms2@gmail.com>
2024-01-20 13:00:41 +0900

    Style: space

--- types/index.ts
+++ types/index.ts
@@ -18,12 +18,12 @@ export interface CarProps {
     class: string
     combination_mpg: number
     cylinders: number
-    displacement:number
-    drive:string
-    fuel_type:string
-    highway_mpg:number
-    make:string
-    model:string
-    transmission:string
-    year:number
+    displacement: number
+    drive: string
+    fuel_type: string
+    highway_mpg: number
+    make: string
+    model: string
+    transmission: string
+    year: number
 }
