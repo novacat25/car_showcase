29b57d37be40923e76c7957ae7c6a70fe46d306c
SpaceFox75 <sparkstorms2@gmail.com>
2024-01-20 11:21:57 +0900

    Feat: init

--- components/index.ts
+++ components/index.ts
@@ -5,9 +5,11 @@ import NavBar from "./NavBar"
 import Footer from "./Footer"
 import SearchBar from "./SearchBar"
 import SearchManufacturer from "./SearchManufacturer"
+import CarCard from "./CarCard"
 
 export {
     Hero,
+    CarCard,
     CustomButton,
     CustomFilter,
     NavBar,
