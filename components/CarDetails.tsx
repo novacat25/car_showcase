e0a8bef8649136ade6b95457bf41ce1866b40da5
NovaCat <sparkstorms2@gmail.com>
2024-01-22 14:02:07 +0900

    Feat: add Transition & Dialog

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import { CarProps } from '@/types'
-import React from 'react'
+import { Dialog, Transition } from '@headlessui/react'
+import React, { Fragment } from 'react'
 
 interface CarDetailsProps {
     isOpen: boolean
@@ -9,7 +12,23 @@ interface CarDetailsProps {
 
 const CarDetails = ({ isOpen, closeModal, car}: CarDetailsProps) => {
   return (
-    <div>CarDetails</div>
+    <>
+        <Transition appear show={isOpen} as={Fragment}>
+            <Dialog as="div" className="relative z-10" onClose={closeModal}>
+                <Transition.Child
+                    as={Fragment}
+                    enter="ease-out duration-300"
+                    enterFrom="opacity-0"
+                    enterTo="opacity-100"
+                    leave="ease-in duration-200"
+                    leaveFrom="opcaity-100"
+                    leaveTo="opacity-0"
+                >
+                    <div className="fixed inset-0 bg-black bg-opcaity-25" />
+                </Transition.Child>
+            </Dialog>
+        </Transition>
+    </>
   )
 }
 
