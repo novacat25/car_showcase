35af132fa93ce8cca991ebd41bebae343ec063e8
NovaCat <37829927+novacat25@users.noreply.github.com>
2024-03-01 11:23:17 +0900

    [Feat]Car-Catalog (#13)
    * Feat: init
    
    * Feat: positions of SearchBar and CustomFilters
    
    * Feat: basic components
    
    * Feat: Headless-UI
    
    * [Feat]SearchBar (#14)
    
    * Feat: init
    
    * Feat: basic component
    
    * style: import route
    
    * Feat: add SearchManufacturer & Props
    
    * Feat: make basic design by using headless-UI
    
    * Feat: add Transition
    
    * Feat: Transition
    
    * Style: definite types of params
    
    * Modify: delete unnecessary code
    
    * Feat: implement autocomplete combobox
    
    * Feat: add props on Combobox component
    
    * [Feat]Fetch API (#16)
    
    * Feat: init
    
    * Test: Server Side Fetch
    
    * Feat: add isDataEmpty
    
    * Feat: Design of error message
    
    * Feat: basic map function
    
    * [Feat]CarCard (#17)
    
    * Feat: init
    
    * Fix: resolve warnings about map function
    
    * Feat: add new interface CarProps
    
    * Style: space
    
    * Feat: implement CarCard.tsx
    
    * Feat: mapping CarCard
    
    * Feat: calculateCarRent
    
    * Feat: display carRent
    
    * Feat: Styling of carCard component
    
    * Feat: add images
    
    * Feat: information of cars
    
    * Fix: remove warnings
    
    * Feat: add CustomButtons on Card components
    
    * Feat: definitions additional styles and props on CustomButton
    
    * Feat: apply style on CarCard
    
    * Style: Indentation
    
    * Style: make it more readable
    
    * [Feat]Details(Modal) (#21)
    
    * Feat: init
    
    * Feat: definite Props
    
    * Feat: add Transition & Dialog
    
    * Feat: design of the basic component
    
    * Fix: quote error
    
    * Feat: title div
    
    * Feat: add preview images
    
    * Feat: a title of car
    
    * Feat: Design of the Modal
    
    * Feat: create generateCarImgeUrl function
    
    * Feat: apply generateCarImageUrl on Components
    
    * Fix: make API_KEY readable on Client Side(&Refactor)

--- components/index.ts
+++ components/index.ts
@@ -1,11 +1,21 @@
 import Hero from "./Hero"
+import CarCard from "./CarCard"
+import CarDetails from "./CarDetails"
 import CustomButton from "./CustomButton"
+import CustomFilter from "./CustomFilter"
 import NavBar from "./NavBar"
 import Footer from "./Footer"
+import SearchBar from "./SearchBar"
+import SearchManufacturer from "./SearchManufacturer"
 
 export {
     Hero,
+    CarCard,
+    CarDetails,
     CustomButton,
+    CustomFilter,
     NavBar,
     Footer,
+    SearchBar,
+    SearchManufacturer,
 }
