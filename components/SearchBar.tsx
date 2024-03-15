a7d514728bc4dcc6fadf759a013529cc0f632777
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-15 11:34:03 +0900

    Feat: handleSearch & updateSearchParam

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,7 @@
 "use client"
 
-import { ChangeEvent, useState } from "react"
+import React, { ChangeEvent, useState } from "react"
+import { useRouter } from "next/navigation"
 import { SearchManufacturer } from "."
 import Image from 'next/image'
 
@@ -19,8 +20,39 @@ const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
 const SearchBar = () => {
     const [manufacturer,setManufacturer] = useState('')
     const [model,setModel] = useState('')
-    const handleSearch = () => {
+    const router = useRouter()
 
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+
+      if(manufacturer === '' && model === '') {
+        return alert('Please fill in the search bar')
+      }
+
+      updateSearchParams(
+        model.toLowerCase(),
+        manufacturer.toLowerCase()
+      )
+    }
+
+    const updateSearchParams = (model: string, manufacturer: string) => {
+      const searchParams = new URLSearchParams(window.location.search)
+
+      if(model) {
+        searchParams.set('model',model)
+      } else {
+        searchParams.delete('model')
+      }
+
+      if(manufacturer) {
+        searchParams.set('manufacturer',manufacturer)
+      } else {
+        searchParams.delete('manufacturer')
+      }
+
+      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
+
+      router.push(newPathname)
     }
 
   return (
