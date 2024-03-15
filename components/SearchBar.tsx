c20b0a2ed5ddfa3015799acda40faaa49ed8317a
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-15 11:36:18 +0900

    Refactor

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,11 +3,11 @@
 import React, { ChangeEvent, useState } from "react"
 import { useRouter } from "next/navigation"
 import { SearchManufacturer } from "."
-import Image from 'next/image'
+import Image from "next/image"
 
-const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
+const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
   <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
-    <Image 
+    <Image
       src="/magnifying-glass.svg"
       alt="magnifying glass"
       width={40}
@@ -18,71 +18,71 @@ const SearchButton = ({ otherClasses }: { otherClasses: string}) => (
 )
 
 const SearchBar = () => {
-    const [manufacturer,setManufacturer] = useState('')
-    const [model,setModel] = useState('')
-    const router = useRouter()
+  const [manufacturer, setManufacturer] = useState("")
+  const [model, setModel] = useState("")
+  const router = useRouter()
 
-    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault()
-
-      if(manufacturer === '' && model === '') {
-        return alert('Please fill in the search bar')
-      }
-
-      updateSearchParams(
-        model.toLowerCase(),
-        manufacturer.toLowerCase()
-      )
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === "" && model === "") {
+      return alert("Please fill in the search bar")
     }
 
-    const updateSearchParams = (model: string, manufacturer: string) => {
-      const searchParams = new URLSearchParams(window.location.search)
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase())
+  };
 
-      if(model) {
-        searchParams.set('model',model)
-      } else {
-        searchParams.delete('model')
-      }
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search)
 
-      if(manufacturer) {
-        searchParams.set('manufacturer',manufacturer)
-      } else {
-        searchParams.delete('manufacturer')
-      }
-
-      const newPathname = `${window.location.pathname}?${searchParams.toString()}`
-
-      router.push(newPathname)
+    if (model) {
+      searchParams.set("model", model)
+    } else {
+      searchParams.delete("model")
     }
+
+    if (manufacturer) {
+      searchParams.set("manufacturer", manufacturer)
+    } else {
+      searchParams.delete("manufacturer")
+    }
+
+    const newPathname = `${
+      window.location.pathname
+    }?${searchParams.toString()}`
+
+    router.push(newPathname)
+  }
 
   return (
     <form className="searchbar" onSubmit={handleSearch}>
-        <div className="searchbar__item">
-            <SearchManufacturer
-                manufacturer={manufacturer}
-                setManufacturer={setManufacturer}
-            />
-            <SearchButton otherClasses="sm:hidden" />
-        </div>
-        <div className="searchbar__item">
-          <Image
-            src="/model-icon.png"
-            width={25}
-            height={25}
-            className="absolute w-[20px] h-[20px] ml-4"
-            alt="car model"
-          />
-          <input
-            type="text"
-            name="model"
-            value={model}
-            onChange={(e: ChangeEvent<HTMLInputElement>)=>setModel(e.target.value)}
-            placeholder="Tiguan"
-            className="searchbar__input"
-          />
-          <SearchButton otherClasses="sm:hidden" />
-        </div>
-        <SearchButton otherClasses="max-sm:hidden" />
+      <div className="searchbar__item">
+        <SearchManufacturer
+          manufacturer={manufacturer}
+          setManufacturer={setManufacturer}
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <div className="searchbar__item">
+        <Image
+          src="/model-icon.png"
+          width={25}
+          height={25}
+          className="absolute w-[20px] h-[20px] ml-4"
+          alt="car model"
+        />
+        <input
+          type="text"
+          name="model"
+          value={model}
+          onChange={(e: ChangeEvent<HTMLInputElement>) =>
+            setModel(e.target.value)
+          }
+          placeholder="Tiguan"
+          className="searchbar__input"
+        />
+        <SearchButton otherClasses="sm:hidden" />
+      </div>
+      <SearchButton otherClasses="max-sm:hidden" />
     </form>
   )
 }
