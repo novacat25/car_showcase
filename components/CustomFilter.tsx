7eec3b7d31bd8b1295b239b0a65020de41d8f688
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-19 14:05:24 +0900

    Feat: define FilterProps

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,10 +1,26 @@
 "use client"
 
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
-const CustomFilter = () => {
+import { Listbox, Transition } from '@headlessui/react'
+import { CustomFilterProps } from '@/types'
+
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
   return (
-    <div>CustomFilter</div>
+    <div className='w-fit'>
+      <Listbox>
+        <div className='relative w-fit z-10'>
+          <Listbox.Button className="custom-filter__btn">
+            <span>
+              Filter
+            </span>
+          </Listbox.Button>
+        </div>
+      </Listbox>
+    </div>
   )
 }
 
