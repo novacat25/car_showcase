b85b63a63159d2e3f7b543f2fd7c34ede0ae8cd4
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-19 15:20:34 +0900

    Feat: design of CustomFilter

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -5,19 +5,59 @@ import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 
 import { Listbox, Transition } from '@headlessui/react'
-import { CustomFilterProps } from '@/types'
+import { CustomFilterProps, OptionProps } from '@/types'
 
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState<OptionProps>(options[0])
+
+
   return (
     <div className='w-fit'>
-      <Listbox>
+      <Listbox value={selected} onChange={(e: OptionProps) => setSelected(e)}>
         <div className='relative w-fit z-10'>
           <Listbox.Button className="custom-filter__btn">
-            <span>
-              Filter
-            </span>
+            <span className='block truncate'>{selected.title}</span>
+            <Image
+              src="/chevron-up-down.svg"
+              width={20}
+              height={20}
+              className="ml-4 object-contain"
+              alt="chevron up down"
+            />
           </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="custom-filter__options">
+              {options.map((option) => (
+                <Listbox.Option
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                    }`
+                  }
+                >
+                  {({ selected }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? 'font-medium' : 'font-normal'
+                        }`}
+                      >
+                        {option.title}
+                      </span>
+                    </>
+                  )}
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
         </div>
       </Listbox>
     </div>
