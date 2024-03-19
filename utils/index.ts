27caf377f55066fd7308c844a321e7d71ed72664
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-19 10:41:58 +0900

    Feat: add SSR search function

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,14 @@
-import { CarProps } from "@/types"
+import { CarProps, FilterProps } from "@/types"
 
-export const fetchCars = async () => {
+export const fetchCars = async (filters: FilterProps) => {
+    const { manufacturer, year, model, limit, fuel } = filters
 
     const headers = new Headers()
 
     headers.append('X-RapidAPI-Key',process.env.X_RAPID_API_KEY || "")
     headers.append('X-RapidAPI-Host',"cars-by-api-ninjas.p.rapidapi.com")
     
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=q3', {
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {
         headers: headers,
     })
 
