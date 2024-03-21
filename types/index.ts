51b0d4010cb3ad8ceab5148ff44a9f15cd0c9c7a
NovaCat <37829927+novacat25@users.noreply.github.com>
2024-03-21 17:04:07 +0900

    [WIP]Feature/show more (#25)
    * Feat: init
    
    * Feat: design of button
    
    * Feat: Show More
    
    * Feat: making a property optional on FilterProps
    
    * Test: add scroll false
    
    * Fix: disable scrolls
    
    * Refactor

--- types/index.ts
+++ types/index.ts
@@ -32,11 +32,11 @@ export interface CarProps {
 }
 
 export interface FilterProps {
-    manufacturer: string
-    year: number
-    fuel: string
-    limit: number
-    model: string
+    manufacturer?: string
+    year?: number
+    fuel?: string
+    limit?: number
+    model?: string
 }
 
 export interface OptionProps {
@@ -47,4 +47,9 @@ export interface OptionProps {
 export interface CustomFilterProps {
     title: string
     options: OptionProps[]
+}
+
+export interface ShowMoreProps {
+    pageNumber: number
+    isNext: boolean
 }
