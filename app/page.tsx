732a6e44d2b7dae6e27a328bd9809849194d6bdd
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-21 17:02:59 +0900

    Refactor

--- app/page.tsx
+++ app/page.tsx
@@ -10,11 +10,11 @@ interface HomeProps {
 
 export default async function Home({ searchParams }: HomeProps) {
   const allCars = await fetchCars({
-    manufacturer: searchParams.manufacturer || '',
+    manufacturer: searchParams.manufacturer || "",
     year: searchParams.year || 2022,
-    fuel: searchParams.fuel || '',
+    fuel: searchParams.fuel || "",
     limit: searchParams.limit || 10,
-    model: searchParams.model || '',
+    model: searchParams.model || "",
   })
   const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars
 
@@ -24,9 +24,7 @@ export default async function Home({ searchParams }: HomeProps) {
 
       <div className="mt-12 padding-x padding-y max-width" id="discover">
         <div className="home__text-container">
-          <h1 className="text-4xl font-extrabold">
-            Car Catalogue
-          </h1>
+          <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
 
@@ -42,23 +40,23 @@ export default async function Home({ searchParams }: HomeProps) {
         {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
-              {allCars?.map((car)=> (
-              <Fragment key={car}>
-                <CarCard car={car} />
-              </Fragment>
+              {allCars?.map((car) => (
+                <Fragment key={car}>
+                  <CarCard car={car} />
+                </Fragment>
               ))}
             </div>
 
-            <ShowMore 
-              pageNumber={(searchParams.limit || 10) / 10} 
-              isNext={(searchParams.limit || 10) > allCars.length} 
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
             />
           </section>
-        ) :(
-        <div className="home__error-container">
-          <h2 className="text-black text-xl font-bold">Oops, no results</h2>
-          <p>{allCars?.message}</p>
-        </div>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
         )}
       </div>
     </main>
