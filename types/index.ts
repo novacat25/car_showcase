9cf895c4b15fee072e0a76e95af7ce3cf98f5ea8
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-21 15:37:55 +0900

    Feat: making a property optional on FilterProps

--- types/index.ts
+++ types/index.ts
@@ -32,11 +32,11 @@ export interface CarProps {
 }
 
 export interface FilterProps {
-    manufacturer: string
-    year: number
-    fuel: string
-    limit: number
-    model: string
+    manufacturer?: string
+    year?: number
+    fuel?: string
+    limit?: number
+    model?: string
 }
 
 export interface OptionProps {
