dbdf6b2ff7b5125fa52f03d63211fd79d566d7fc
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-21 13:39:01 +0900

    Feat: CustomFilter function

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,23 +1,35 @@
 "use client"
 
-import React, { Fragment, useState } from 'react'
-import Image from 'next/image'
-import { useRouter } from 'next/navigation'
+import React, { Fragment, useState } from "react"
+import Image from "next/image"
+import { useRouter } from "next/navigation"
 
-import { Listbox, Transition } from '@headlessui/react'
-import { CustomFilterProps, OptionProps } from '@/types'
-
+import { Listbox, Transition } from "@headlessui/react"
+import { CustomFilterProps, OptionProps } from "@/types"
+import { updateSearchParams } from "@/utils"
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState<OptionProps>(options[0])
+  const router = useRouter()
 
+  const handleUpdateParams = (e: { title: string, value: string }) => {
+    const newPathName = updateSearchParams(title, e.value.toLowerCase())
+
+    router.push(newPathName)
+  }
 
   return (
-    <div className='w-fit'>
-      <Listbox value={selected} onChange={(e: OptionProps) => setSelected(e)}>
-        <div className='relative w-fit z-10'>
+    <div className="w-fit">
+      <Listbox
+        value={selected}
+        onChange={(e: OptionProps) => {
+          setSelected(e)
+          handleUpdateParams(e)
+        }}
+      >
+        <div className="relative w-fit z-10">
           <Listbox.Button className="custom-filter__btn">
-            <span className='block truncate'>{selected.title}</span>
+            <span className="block truncate">{selected.title}</span>
             <Image
               src="/chevron-up-down.svg"
               width={20}
@@ -39,7 +51,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                   value={option}
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 px-4 ${
-                      active ? 'bg-primary-blue text-white' : 'text-gray-900'
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
                     }`
                   }
                 >
@@ -47,7 +59,7 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
                     <>
                       <span
                         className={`block truncate ${
-                          selected ? 'font-medium' : 'font-normal'
+                          selected ? "font-medium" : "font-normal"
                         }`}
                       >
                         {option.title}
