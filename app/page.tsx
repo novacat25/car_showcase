adff4a82fab69b41eac5c63a93b42fb033f80778
SpaceFox75 <sparkstorms2@gmail.com>
2024-03-25 10:34:14 +0900

    Fix: capitalize

--- app/page.tsx
+++ app/page.tsx
@@ -60,8 +60,8 @@ export default function Home() {
           <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
 
           <div className="home__filter-container">
-            <CustomFilter title="fuel" options={fuels} setFilter={setFuel} />
-            <CustomFilter title="year" options={yearsOfProduction} setFilter={setYear} />
+            <CustomFilter title="Fuel" options={fuels} setFilter={setFuel} />
+            <CustomFilter title="Year" options={yearsOfProduction} setFilter={setYear} />
           </div>
         </div>
 
